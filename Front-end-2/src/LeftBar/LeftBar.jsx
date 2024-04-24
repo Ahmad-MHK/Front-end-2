cb7554c54c8e4b6bf9cb4772a4251724e1868024
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2024-04-24 14:28:20 +0200

    LeftSideBar
    LeftSideBar with no css

---
+++ Front-end-2/src/LeftBar/LeftBar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './LeftBar.css'
+
+function LeftBar() {
+
+  return (
+    <>
+    <div className="Block">
+      <div className="Main-block">
+        <img src="" alt="" />
+        <h2>Sportagram</h2>
+      </div>
+      <div className="Slaves-block">
+        <img src="" alt="" />
+        <h3>Home</h3>
+      </div>
+      <div className="Slaves-block">
+        <img src="" alt="" />
+        <h3>Search</h3>
+      </div>
+      <div className="Slaves-block">
+        <img src="" alt="" />
+        <h3>Explore</h3>
+      </div>
+      <div className="Slaves-block">
+        <img src="" alt="" />
+        <h3>Message</h3>
+      </div>
+      <div className="Slaves-block">
+        <img src="" alt="" />
+        <h3>Create</h3>
+      </div>
+      <div className="Slaves-block">
+        <img src="" alt="" />
+        <h3>Profile</h3>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default LeftBar
