0dc5ebd623047e8724dca3ac598633dfd67cf1ab
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2024-05-15 13:50:56 +0200

    TopBar
    basic is finished

--- Front-end-2/src/App.jsx
+++ Front-end-2/src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LeftBar from './LeftBar/LeftBar'
+import Topbar from './RightSide/TopBar'
 import ConnectionFirebase from "./Firebase/FirebaseConfig"
 
 function App() {
 
   return (
-    <>
+    <div className="row">
       <LeftBar></LeftBar>
-    </>
+      <Topbar></Topbar>
+    </div>
   )
 }
 
