54349dea4fd8355a0f260b4229d78532ede78728
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2024-05-15 13:31:14 +0200

    Leftside
    need to add img and links

--- Front-end-2/src/LeftBar/LeftBar.jsx
+++ Front-end-2/src/LeftBar/LeftBar.jsx
@@ -12,28 +12,40 @@ function LeftBar() {
         <h2>Sportagram</h2>
       </div>
       <div className="Slaves-block">
+        <a href="" className='text'>
         <img src="" alt="" />
         <h3>Home</h3>
+        </a>
       </div>
       <div className="Slaves-block">
+        <a href="" className='text'>
         <img src="" alt="" />
         <h3>Search</h3>
+        </a>
       </div>
       <div className="Slaves-block">
+        <a href="" className='text'>
         <img src="" alt="" />
         <h3>Explore</h3>
+        </a>
       </div>
       <div className="Slaves-block">
+        <a href="" className='text'>
         <img src="" alt="" />
         <h3>Message</h3>
+        </a>
       </div>
       <div className="Slaves-block">
+        <a href="" className='text'>
         <img src="" alt="" />
         <h3>Create</h3>
+        </a>
       </div>
       <div className="Slaves-block">
+        <a href="" className='text'>
         <img src="" alt="" />
         <h3>Profile</h3>
+        </a>
       </div>
     </div>
     </>
